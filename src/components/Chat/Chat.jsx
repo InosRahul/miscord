b5b81ad43c90fbb4506e8e30f47695d93c20da43
Rahul Soni <rahulsoni2225@gmail.com>
2021-09-10 17:29:51 +0530

    updated: Functionality to create, select and delete chats

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,18 +1,28 @@
-import { useChat } from 'context/ChatContext';
 import { useEffect } from 'react';
+import { useChat } from 'context';
 import { getChats, ChatEngine } from 'react-chat-engine';
-import { LeftSidebar } from 'components';
+import { LeftSidebar, ChatToolbar, ChatInput, MessageList } from 'components';
 
 export const Chat = () => {
-  const { myChats, setMyChats, chatConfig, selectedChat } = useChat();
+  const {
+    myChats,
+    setMyChats,
+    chatConfig,
+    selectedChat,
+    selectChatClick,
+    setSelectedChat,
+  } = useChat();
 
   useEffect(() => {
     console.log('My Chats: ', myChats);
   }, [myChats]);
 
+  useEffect(() => {
+    console.log('Selected Chat: ', selectedChat);
+  }, [selectedChat]);
+
   return (
     <>
-      <LeftSidebar />
       {!!chatConfig && (
         <ChatEngine
           hideUI={true}
@@ -22,13 +32,49 @@ export const Chat = () => {
           onConnect={() => {
             getChats(chatConfig, setMyChats);
           }}
+          onNewChat={chat => {
+            if (chat.admin.username === chatConfig.userName) {
+              selectChatClick(chat);
+            }
+            setMyChats([...myChats, chat].sort((a, b) => a.id - b.id));
+          }}
+          onDeleteChat={chat => {
+            if (selectedChat?.id === chat.id) {
+              setSelectedChat(null);
+            }
+            setMyChats(
+              myChats.filter(c => c.id !== chat.id).sort((a, b) => a.id - b.id),
+            );
+          }}
+          onNewMessage={(chatId, message) => {
+            if (selectedChat && chatId === selectedChat.id) {
+              setSelectedChat({
+                ...selectedChat,
+                messages: [...selectedChat.messages, message],
+              });
+            }
+            const chatThatMessageBelongsTo = myChats.find(c => c.id === chatId);
+            const filteredChats = myChats.filter(c => c.id !== chatId);
+            const updatedChat = {
+              ...chatThatMessageBelongsTo,
+              last_message: message,
+            };
+            setMyChats(
+              [updatedChat, ...filteredChats].sort((a, b) => a.id - b.id),
+            );
+          }}
         />
       )}
 
       <div className="chat-container">
+        <LeftSidebar />
         <div className="current-chat">
           {selectedChat ? (
-            <></>
+            <div className="chat">
+              <ChatToolbar />
+              <MessageList />
+              <ChatInput />
+            </div>
           ) : (
             <div className="no-chat-selected">Select A Chat</div>
           )}
